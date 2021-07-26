a90004725ce7c3bf0ff34780f834e0ce15973145
JoaoNoel <67752072+JoaoNoel@users.noreply.github.com>
2021-07-26 15:08:03 +0000

    alter css tailwindcss

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -10,10 +10,12 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
     return (
-        <div>
+        <div className={`flex h-screen w-screen `}>
             <MenuLateral />
-            <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
-            <Conteudo> {props.children}</Conteudo>
+            <div className={`flex flex-col bg-red-400`}>
+                <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
+                <Conteudo> {props.children}</Conteudo>
+            </div>
         </div>
     )
 }
