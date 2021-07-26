c66e040fb4561c829bb2564c0320511000c0fa4d
JoaoNoel <67752072+JoaoNoel@users.noreply.github.com>
2021-07-26 12:42:58 +0000

    subindo os ficheiros de components

--- src/components/template/AvatarUsuario.tsx
+++ src/components/template/AvatarUsuario.tsx
@@ -1,3 +1,5 @@
-export default class AvatarUsuario {
-
+export default function AvatarUsuario() {
+    return (
+        <div></div >
+    )
 }
---
+++ src/components/template/Cabecalho.tsx
@@ -0,0 +1,12 @@
+interface CabecalhoProps {
+    titulo: string
+    subtitulo: string
+}
+
+export default function Layout(props: CabecalhoProps) {
+    return (
+        <div>
+
+        </div>
+    )
+}
