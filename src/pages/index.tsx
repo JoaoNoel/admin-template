f0a41bd2e47b94850f1a71e58e0581a24003b1b7
JoaoNoel <67752072+JoaoNoel@users.noreply.github.com>
2021-07-26 12:24:57 +0000

    sgundo commit pasta src e componentes

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello Word! Bem vindo ao Nextjs para criação do template de administração Padrão para os futuros rpojectos!</h1>
+    </div>
+  )
+}
